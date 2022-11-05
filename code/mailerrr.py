34a1bbbd3b2d29dbf8fda1334fb300c453937016
RackBoy <73978995+RackBoy@users.noreply.github.com>
2022-11-04 20:20:52 -0500

    Update mailerrr.py

--- code/mailerrr.py
+++ code/mailerrr.py
@@ -1,30 +1,84 @@
-'''
-this class is only used with
-passGeneratorSendMail in order to send
-passwords generated to an especific mail address
-when its required
-'''
-# old port 587
-import smtplib 
-import ssl 
-from email.message import EmailMessage
+from mailer import Mailer
 
 class mailer:
 	def __init__(self, SENDER_EMAIL, SENDER_PASS):
 		self.SENDER_EMAIL = SENDER_EMAIL
 		self.SENDER_PASS = SENDER_PASS
+		#Mailer(self.SENDER_EMAIL, self.SENDER_PASS)
 
 	def send_mail(self, receptor, rsubject, rcontent):
 		self.receptor = receptor
 		self.rsubject = rsubject
 		self.rcontent = rcontent
-		email = EmailMessage()
-		email['from'] = self.SENDER_EMAIL
-		email['to'] = self.receptor
-		email['subject'] = self.rsubject
-		email.set_content(self.rcontent)
-		contexto = ssl.create_default_context()
-		with smtplib.SMTP_SSL('smtp.gmail.com', port = 465, context = contexto) as smtp:
-			smtp.login(self.SENDER_EMAIL, self.SENDER_PASS)
-			smtp.sendmail(self.SENDER_EMAIL, self.receptor, email.as_string())
-			print(f"email send to {self.receptor}")
+
+		mail = Mailer(self.SENDER_EMAIL, self.SENDER_PASS)
+
+		mail.send (receiver = self.receptor,
+        subject = self.rsubject,
+        message = self.rcontent)
+	
+	def send_file(self, receptor, rsubject, rcontent, file):
+		self.receptor = receptor
+		self.rsubject = rsubject
+		self.rcontent = rcontent
+		self.file = file
+
+		mail = Mailer(self.SENDER_EMAIL, self.SENDER_PASS)
+
+		mail.send (receiver = self.receptor,
+        subject = self.rsubject,
+        message = self.rcontent,
+        files = self.file)
+		 #image="angry.png")
+
+	def send_image(self, receptor, rsubject, rcontent, file):
+		self.receptor = receptor
+		self.rsubject = rsubject
+		self.rcontent = rcontent
+		self.file = file
+
+		mail = Mailer(self.SENDER_EMAIL, self.SENDER_PASS)
+
+		mail.send (receiver = self.receptor,
+        subject = self.rsubject,
+        message = self.rcontent,
+		image = self.file) 
+
+	def send_audio(self, receptor, rsubject, rcontent, file):
+		self.receptor = receptor
+		self.rsubject = rsubject
+		self.rcontent = rcontent
+		self.file = file
+
+		mail = Mailer(self.SENDER_EMAIL, self.SENDER_PASS)
+
+		mail.send (receiver = self.receptor,
+        subject = self.rsubject,
+        message = self.rcontent,
+		audio = self.file)  
+
+	#this method allows to send multiples type of files, audio, file and image at once
+	def send_stuff(self, receptor, rsubject, rcontent, file, img, audio):
+		self.receptor = receptor
+		self.rsubject = rsubject
+		self.rcontent = rcontent
+		self.file = file
+		self.img = img
+		self.audio = audio
+
+		mail = Mailer(self.SENDER_EMAIL, self.SENDER_PASS)
+
+		mail.send (receiver = self.receptor,
+        subject = self.rsubject,
+        message = self.rcontent,
+		image = self.img,  
+		audio = self.audio, 
+		file = self.file )
+
+	def multi_mail(self, multi):
+		self.multi = multi
+		mail = Mailer(self.SENDER_EMAIL, self.SENDER_PASS)
+		if(multi):
+			mail.settings(multi=True)
+		else:
+			return
